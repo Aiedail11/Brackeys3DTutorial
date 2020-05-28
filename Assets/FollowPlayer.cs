20aa2d34fb6a16c76359797b5f0245b418e628ba
= <emily.haggard@exelaration.com>
2020-05-28 17:58:37 -0400

    Finished Episode 5

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+	public Transform player;
+	public Vector3 offset;
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = player.position + offset; //lowercase transform references the transform component of the current object
+	
+	}
+}
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,6 +4,9 @@ public class PlayerMovement : MonoBehaviour {
 
 	public Rigidbody rb;
 
+	public float forwardForce = 2000f;
+	public float sidewaysForce = 500f;
+
 	// Use this for initialization
 	void Start () {
 
@@ -11,6 +14,19 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		rb.AddForce(0, 0, 200 * Time.deltaTime);
+		
+	}
+
+	// FixedUpdate is better for physics stuff, but is sometimes not responsive enough
+	// for certain types of input, such as jumping
+	void FixedUpdate(){
+		rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+		if(Input.GetKey("d")){
+			rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+		}
+		if(Input.GetKey("a")){
+			rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+		}
 	}
 }
